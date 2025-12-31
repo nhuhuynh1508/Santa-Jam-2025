e32cf34e9f47bb5b9681db6e505e18405c76c464
Tsumiki <93415945+nhuhuynh1508@users.noreply.github.com>
2025-12-31 21:16:40 +0700

    Update main

--- src/main.js
+++ src/main.js
@@ -1,35 +1,23 @@
-import { Application, Assets, Sprite } from "pixi.js";
+import { Application } from "pixi.js";
+
+import { DialogueScreen } from "./progress/dialogueScreen";
+import { StoryScreen } from "./progress/storyScreen";
+import { SceneManager } from "./screenManager";
 
 (async () => {
   // Create a new application
   const app = new Application();
 
   // Initialize the application
-  await app.init({ background: "#1099bb", resizeTo: window });
+  await app.init({ background: "#333333ff", height: 540, width: 540 });
 
   // Append the application canvas to the document body
   document.getElementById("pixi-container").appendChild(app.canvas);
 
-  // Load the bunny texture
-  const texture = await Assets.load("/assets/bunny.png");
+  SceneManager.init(app);
 
-  // Create a bunny Sprite
-  const bunny = new Sprite(texture);
-
-  // Center the sprite's anchor point
-  bunny.anchor.set(0.5);
-
-  // Move the sprite to the center of the screen
-  bunny.position.set(app.screen.width / 2, app.screen.height / 2);
-
-  // Add the bunny to the stage
-  app.stage.addChild(bunny);
-
-  // Listen for animate update
-  app.ticker.add((time) => {
-    // Just for fun, let's rotate mr rabbit a little.
-    // * Delta is 1 if running at 100% performance *
-    // * Creates frame-independent transformation *
-    bunny.rotation += 0.1 * time.deltaTime;
-  });
+  SceneManager.changeScene(() => StoryScreen(app, () => {
+      // after story scene ends, load dialogue scene
+      SceneManager.changeScene(() => DialogueScreen(app));
+  }));
 })();
