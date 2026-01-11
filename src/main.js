35c42f8f1a8e6ebcbc64145b85cd2de09c99d88d
BillionaireP <96333266+phuhuynhlean@users.noreply.github.com>
2026-01-11 16:18:18 +0700

    feat: reorganize main file

--- src/main.js
+++ src/main.js
@@ -1,28 +1,20 @@
 import { Application } from "pixi.js";
-
 import { DialogueScreen } from "./progress/dialogueScreen";
 import { StoryScreen } from "./progress/storyScreen";
 import { SceneManager } from "./screenManager";
 
-  (async () => {
-    // Create a new application
-    const app = new Application();
-  (async () => {
-    // Create a new application
-    const app = new Application();
-
-  // Initialize the application
+async function main() {
+  const app = new Application();
   await app.init({ background: "#333333ff", height: 540, width: 540 });
-
-    // Append the application canvas to the document body
-    document.getElementById("pixi-container").appendChild(app.canvas);
-    // Append the application canvas to the document body
-    document.getElementById("pixi-container").appendChild(app.canvas);
-
+  
+  document.getElementById("pixi-container").appendChild(app.canvas);
+  document.getElementById("pixi-container").appendChild(app.canvas);
+  
   SceneManager.init(app);
-
+  
   SceneManager.changeScene(() => StoryScreen(app, () => {
-      // after story scene ends, load dialogue scene
-      SceneManager.changeScene(() => DialogueScreen(app));
+    SceneManager.changeScene(() => DialogueScreen(app));
   }));
-})();
+}
+
+main();
