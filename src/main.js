a55849df5f716505cceb0c816b2fa537a31daa34
BillionaireP <96333266+phuhuynhlean@users.noreply.github.com>
2026-01-11 16:09:45 +0700

    feat: resolve merge conflicts

--- src/main.js
+++ src/main.js
@@ -4,15 +4,15 @@ import { DialogueScreen } from "./progress/dialogueScreen";
 import { StoryScreen } from "./progress/storyScreen";
 import { SceneManager } from "./screenManager";
 
-(async () => {
-  // Create a new application
-  const app = new Application();
+  (async () => {
+    // Create a new application
+    const app = new Application();
 
   // Initialize the application
   await app.init({ background: "#333333ff", height: 540, width: 540 });
 
-  // Append the application canvas to the document body
-  document.getElementById("pixi-container").appendChild(app.canvas);
+    // Append the application canvas to the document body
+    document.getElementById("pixi-container").appendChild(app.canvas);
 
   SceneManager.init(app);
 
