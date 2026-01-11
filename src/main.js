ee08ce1760ce76584ae4978e300c4250df31f64f
BillionaireP <96333266+phuhuynhlean@users.noreply.github.com>
2026-01-11 16:10:52 +0700

    feat: resolve merge conflict

--- src/main.js
+++ src/main.js
@@ -7,10 +7,15 @@ import { SceneManager } from "./screenManager";
   (async () => {
     // Create a new application
     const app = new Application();
+  (async () => {
+    // Create a new application
+    const app = new Application();
 
   // Initialize the application
   await app.init({ background: "#333333ff", height: 540, width: 540 });
 
+    // Append the application canvas to the document body
+    document.getElementById("pixi-container").appendChild(app.canvas);
     // Append the application canvas to the document body
     document.getElementById("pixi-container").appendChild(app.canvas);
 
